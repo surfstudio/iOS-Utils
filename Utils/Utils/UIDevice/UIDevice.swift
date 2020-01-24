7078c5af5510e33b48bd46f9b7f563cf2dda2b0d
Krupenko Validislav <dev.fixique@gmail.com>
2020-01-24 17:25:32 +0300

    Fix code formatting

--- Utils/Utils/UIDevice/UIDevice.swift
+++ Utils/Utils/UIDevice/UIDevice.swift
@@ -12,32 +12,40 @@ public extension UIDevice {
 
     /// Returns 'true' if device have 4inch diagonal screen or smaller (5, 5s, 5c, SE)
     static var isSmallPhone: Bool {
-        let smallSizes: [Size] = [.screen3_5Inch,
-                                  .screen4Inch]
+        let smallSizes: [Size] = [
+            .screen3_5Inch,
+            .screen4Inch
+        ]
         return smallSizes.contains(Device.size())
     }
 
     /// Returns `true` if device have screen size of X version
     static var isXPhone: Bool {
-        let xSizes: [Size] = [.screen5_8Inch,
-                              .screen6_1Inch,
-                              .screen6_5Inch]
+        let xSizes: [Size] = [
+            .screen5_8Inch,
+            .screen6_1Inch,
+            .screen6_5Inch
+        ]
         return xSizes.contains(Device.size())
     }
 
     /// Returns 'true' if device have 4.7inch and 5.5inch diagonal screen (6/7/8 normal  or '+')
     static var isNormalPhone: Bool {
-        let normalSizes: [Size] = [.screen4_7Inch,
-                                   .screen5_5Inch]
+        let normalSizes: [Size] = [
+            .screen4_7Inch,
+            .screen5_5Inch
+        ]
         return normalSizes.contains(Device.size())
     }
 
     /// Returns 'true' if current device is iPad
     static var isPad: Bool {
-        let padSizes: [Size] = [.screen7_9Inch,
-                                .screen9_7Inch,
-                                .screen10_5Inch,
-                                .screen12_9Inch]
+        let padSizes: [Size] = [
+            .screen7_9Inch,
+            .screen9_7Inch,
+            .screen10_5Inch,
+            .screen12_9Inch
+        ]
         return padSizes.contains(Device.size())
     }
 
