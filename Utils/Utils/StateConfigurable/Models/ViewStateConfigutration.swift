94597c80bf23b140cf933ed12c48bd0ae7fddd67
Никита Гагаринов <gagarinov@surfstudio.ru>
2020-10-30 17:56:34 +0300

    fix ViewStateConfiguration to public.

--- Utils/Utils/StateConfigurable/Models/ViewStateConfigutration.swift
+++ Utils/Utils/StateConfigurable/Models/ViewStateConfigutration.swift
@@ -9,9 +9,22 @@
 import Foundation
 
 public struct ViewStateConfiguration {
-    var actionStyle: UIStyle<UIButton>
-    var messageStyle: UIStyle<UILabel>
-    var iconWidth: CGFloat
-    var messageTopOffset: CGFloat
-    var icon: UIImage
+    public let actionStyle: UIStyle<UIButton>
+    public let messageStyle: UIStyle<UILabel>
+    public let iconWidth: CGFloat
+    public let messageTopOffset: CGFloat
+    public let icon: UIImage
+
+    public init(actionStyle: UIStyle<UIButton>,
+                messageStyle: UIStyle<UILabel>,
+                iconWidth: CGFloat,
+                messageTopOffset: CGFloat,
+                icon: UIImage) {
+        self.actionStyle = actionStyle
+        self.messageStyle = messageStyle
+        self.iconWidth = iconWidth
+        self.messageTopOffset = messageTopOffset
+        self.icon = icon
+    }
+
 }
