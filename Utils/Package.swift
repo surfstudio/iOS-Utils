fb9e7765c4975344d4033ffb1e233c4b1f05bf61
Alena Belyaeva <alenko31@gmail.com>
2020-12-03 19:19:26 +0300

    add package manifest

---
+++ Utils/Package.swift
@@ -0,0 +1,39 @@
+// swift-tools-version:5.3
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "Utils",
+    platforms: [.iOS(.v11)],
+    products: [
+        .library(
+            name: "Utils",
+            targets: ["Utils"]),
+    ],
+    dependencies: [
+        .package(
+            name: "CryptoSwift",
+            url: "https://github.com/krzyzanowskim/CryptoSwift",
+            .exact("1.3.1")
+            )
+        ],
+    targets: [
+        .target(
+            name: "Utils",
+            dependencies: ["CryptoSwift"],
+            path: "Utils",
+            exclude: [
+                "Info.plist"
+            ]
+        ),
+        .testTarget(
+            name: "UtilsTests",
+            dependencies: ["Utils"],
+            path: "UtilsTests",
+            exclude: [
+                "Info.plist"
+            ]
+        )
+    ]
+)
