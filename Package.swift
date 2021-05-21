55e5b9c8d721fd99626c9233fdf65ab5b21df049
Alena Belyaeva <alenko31@gmail.com>
2021-05-21 17:24:42 +0300

    cryptoSwift version update to 1.4

--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let package = Package(
         .package(
             name: "CryptoSwift",
             url: "https://github.com/krzyzanowskim/CryptoSwift",
-            .exact("1.3.1")
+            .exact("1.4.0")
         ),
         .package(
             name: "Device",
