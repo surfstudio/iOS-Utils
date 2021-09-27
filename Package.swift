6a07ac4e638981715512329596dcc42f4c4ee8de
Alena Belyaeva <alenko31@gmail.com>
2021-09-27 20:17:45 +0300

    cryptoSwift pod version 1.4.0

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 
 let package = Package(
     name: "Utils",
-    platforms: [.iOS(.v11)],
+    platforms: [.iOS(.v11), .macOS(.v10_12)],
     products: [
         .library(
             name: "Utils",
