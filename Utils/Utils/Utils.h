0253c7ea9c795bc2d87787d275e6d5f31dbfc890
Alexander <chausov@surfstudio.ru>
2022-06-01 19:14:16 +0300

    IDPT-967 updates for github actions

--- Utils/Utils/Utils.h
+++ Utils/Utils/Utils.h
@@ -6,7 +6,7 @@
 //  Copyright © 2018 Surf. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
 
 //! Project version number for Utils.
 FOUNDATION_EXPORT double UtilsVersionNumber;
