94c470fc3333b1d9cc6db698d706e565f338a078
Alexander <chausov@surfstudio.ru>
2022-06-09 12:36:40 +0300

    IDPT-976 added example project

---
+++ UtilsExample/UtilsExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UtilsExample
+//
+//  Created by chausov on 09.06.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
